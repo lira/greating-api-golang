b76d5a284201584d011ba2988e637ad7e8d276c3
Fernando Lira <fernando.lira@mercadolivre.com>
2020-12-03 16:41:29 -0300

    init set informations

--- main.go
+++ main.go
@@ -4,9 +4,11 @@ import "github.com/gin-gonic/gin"
 
 func main() {
 	r := gin.Default()
-	r.GET("/", addCors, func(c *gin.Context) {
+	r.GET("/greeting", addCors, func(c *gin.Context) {
 		c.JSON(200, gin.H{
 			"greeting": "Olá!",
+			"owner": "Fernando Lira",
+			"repository": "https://github.com/lira/greeting-api-golang",
 		})
 	})
 	r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
